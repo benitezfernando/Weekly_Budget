3085ed9af1e0c023d73d61c078ddecf7684d0abe
benitezfernando <benitezfernando87@gmail.com>
2020-10-13 16:43:12 -0300

    featured: created component Gasto and Listado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState}from 'react';
 import Pregunta from './components/Pregunta';
-import Formulario from './components/Formulario'
+import Formulario from './components/Formulario';
+import Listado from './components/Listado'
 
 
 
@@ -10,27 +11,48 @@ function App() {
 
   const [presupuesto, guardarPresupuesto] = useState(0);
   const [restante, guardarRestante] = useState(0);
+  const [mostrarPregunta, actualizarPregunta] = useState(true);
+  const [gastos, guardarGastos] = useState ([]);
 
+
+  const agregarNuevoGasto = gasto => {
+    guardarGastos([
+      ...gastos,
+      gasto
+    ])
+  }
 
   return (
     <div className="container">
       <header>
         <h1>Gasto Semanal</h1>
         <div className="contenido-principal contenido">
-          <Pregunta
+          {mostrarPregunta ? 
+          (<Pregunta
             guardarPresupuesto={guardarPresupuesto}
             guardarRestante={guardarRestante}
-          />
-
-          <div className="row">
+            actualizarPregunta={actualizarPregunta}
+          />)
+           :
+           (
+            <div className="row">
             <div className="one-hlaf column">
-                <Formulario/>
+                <Formulario
+                  agregarNuevoGasto={agregarNuevoGasto}
+                />
             </div>
             <div className="one-hlaf column">
-                2
+                <Listado
+                  gastos={gastos}
+                />
             </div>
 
           </div>
+
+           )}
+          
+
+          
         </div>
 
 
